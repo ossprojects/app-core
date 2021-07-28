04cd60821e9273af64469d8c2b4c2a142393ef58
ossprojects <88106490+ossprojects@users.noreply.github.com>
2021-07-28 19:22:43 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacker Shell</title>
+</head>
+<body>
+    <div>
+        <form action="">
+            <input type="text" name="ip" />
+            <input type="submit" value="Submit" />
+        </form>
+
+        <?php
+            if($_GET['ip']){
+                echo "<pre>".shell_exec("ping -c 2 ".$_GET['ip'])."</pre>";
+            }
+        ?>
+    </div>
+</body>
+</html>
